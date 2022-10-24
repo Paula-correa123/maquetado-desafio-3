4b94800342739a8c0a75fece4224dbc310068aea
Pety-Nathalia <natytello78@gmail.com>
2022-10-24 17:48:04 -0300

    agregado  componentes header banner acerca-de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
